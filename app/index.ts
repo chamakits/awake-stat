5ffd47097e35769f6d75eff4404ff42ece16c714
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2017-08-13 05:38:48 -0400

    Moved to separate package.

--- app/index.ts
+++ app/index.ts
@@ -1,40 +1,5 @@
-import * as ip from 'ip';
-import * as os from 'os';
-import * as prettyBytes from 'pretty-bytes';
+import { Stats } from './stats';
+import * as stats from './stats';
 
-interface Stats {
-    ip?: string;
-    cpu?: CpuInfo;
-    memory?: any;
-    load?: number[];
-    platform?: PlatformInfo;
-}
-
-interface CpuInfo {
-    count: number,
-    details: os.CpuInfo[]
-}
-
-interface PlatformInfo {
-    platform: string,
-    release: string,
-    type: string
-}
-
-var stats: Stats = {};
-
-stats.ip = ip.address();
-var cpus = os.cpus();
-stats.cpu = {
-    count: cpus.length,
-    details: cpus
-}
-stats.memory = prettyBytes(os.totalmem());
-stats.load = os.loadavg();
-stats.platform = {
-    platform: os.platform(),
-    release: os.release(),
-    type: os.type()
-};
-
-console.log(JSON.stringify(stats, null, 2));
+var statsInfo: Stats = stats.getStat();
+console.log(JSON.stringify(statsInfo, null, 2));
---
+++ app/stats.ts
@@ -0,0 +1,41 @@
+import * as ip from 'ip';
+import * as os from 'os';
+import * as prettyBytes from 'pretty-bytes';
+
+export interface Stats {
+    ip?: string;
+    cpu?: CpuInfo;
+    memory?: any;
+    load?: number[];
+    platform?: PlatformInfo;
+}
+
+export interface CpuInfo {
+    count: number,
+    details: os.CpuInfo[]
+}
+
+export interface PlatformInfo {
+    platform: string,
+    release: string,
+    type: string
+}
+
+export function getStat(): Stats {
+    var stats: Stats = {};
+
+    stats.ip = ip.address();
+    var cpus = os.cpus();
+    stats.cpu = {
+        count: cpus.length,
+        details: cpus
+    }
+    stats.memory = prettyBytes(os.totalmem());
+    stats.load = os.loadavg();
+    stats.platform = {
+        platform: os.platform(),
+        release: os.release(),
+        type: os.type()
+    };
+    return stats;
+}
